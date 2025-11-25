9748b9eaebc2af75d3709fe2aa710b289014c8b0
JulienML <julien-devos@live.fr>
2025-11-25 18:32:31 +0100

    update LLM Wrapper

--- src/models/llm.py
+++ src/models/llm.py
@@ -3,18 +3,23 @@ from mistralai import Mistral
 class MistralWrapper:
     def __init__(self, api_key: str = None):
         self.client = Mistral(api_key=api_key)
-        self.model = "mistral-small-latest"
-        with open("source/data/prompts/system_prompt.txt", "r") as f:
+        self.model = "mistral-small-2506"
+        with open("./src/data/prompts/system_prompt.txt", "r") as f:
             self.system_prompt = f.read()
             f.close()
 
-    def generate(self, user_prompt: str, history: list[dict[str:str]] = None) -> str:
-        if history is None:
+    def generate(self, user_prompt: str, history: list[dict[str:str]] = []) -> str:
+        if history == []:
             history = [{"role": "system", "content": self.system_prompt}]
+        history.append({"role": "user", "content": user_prompt})
         
         response = self.client.chat.complete(
             model=self.model,
-            messages=history + [{"role": "user", "content": user_prompt}],
+            messages=history,
         )
+
+        text_response = response.choices[0].message.content
+
+        history.append({"role": "assistant", "content": text_response})
         
-        return response.text
+        return text_response, history
